244ed415c93e2f14ea03e73fda87ece5054476be
Viktor <victorkopan98@gmail.com>
2022-06-26 12:19:47 +0600

    feat: created start template

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { FC } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+  const options = {
+    method: "GET",
+    url: "https://movie-database-alternative.p.rapidapi.com/",
+    params: { s: "blade runner", r: "json", page: "1" },
+    headers: {
+      "X-RapidAPI-Key": "51dac984c0msh6298306414999f7p1bd150jsn14acc49ea9df",
+      "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  return <div className="App">111</div>;
+};
 
 export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import reducers from "./reducers";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+  reducer: {},
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware();
   },
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export type AppDispatch = typeof store.dispatch;
