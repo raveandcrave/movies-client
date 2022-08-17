d32ff537aa1b75707cdc837d68f7a22d8657d3a7
Viktor Kopan <victorkopan98@gmail.com>
2022-08-17 18:39:50 +0600

    feat: created response interceptor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import {useState} from 'react';
+import {$authHost} from '../services/http';
+import {User} from '../types';
 
 const HomePage = () => {
-  return <div>Home</div>;
+  const [users, setUsers] = useState<User[]>([]);
+
+  const getUsers = () => {
+    $authHost.get<User[]>('/users').then((res) => setUsers(res.data));
+  };
+
+  return (
+    <>
+      <button onClick={() => getUsers()}>get users</button>
+      {users.map((user) => {
+        return <div key={user.email}>{user.email}</div>;
+      })}
+    </>
+  );
 };
 
 export default HomePage;
--- src/services/http/index.ts
+++ src/services/http/index.ts
@@ -1,4 +1,5 @@
 import axios, {AxiosRequestConfig} from 'axios';
+import {refresh} from '../userAPI';
 
 const $host = axios.create({
   withCredentials: true,
@@ -22,5 +23,25 @@ const authInterceptor = (config: AxiosRequestConfig): AxiosRequestConfig => {
 //TODO: добавить интерцепторы на ошибки и разбить логику по кодам ошибок(код 401 --- редирект на стр логина и тд)
 
 $authHost.interceptors.request.use(authInterceptor);
+
+$authHost.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await refresh();
+        localStorage.setItem('token', response.data.accessToken);
+
+        return $authHost.request(originalRequest);
+      } catch {
+        console.error('Не авторизован');
+      }
+    }
+    throw error;
+  }
+);
 
 export {$host, $authHost};
